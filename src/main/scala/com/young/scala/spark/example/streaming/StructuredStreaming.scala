98627d092cc3b40215cfa773208d526c6bcb67a4
yangyong <398901650@qq.com>
2016-08-01 22:45:28 +0800

    structuredStreaming

--- src/main/scala/com/young/scala/spark/example/streaming/StructuredStreaming.scala
+++ src/main/scala/com/young/scala/spark/example/streaming/StructuredStreaming.scala
@@ -1,13 +1,19 @@
 package com.young.scala.spark.example.streaming
 import org.apache.spark.sql.SparkSession
 
-
 /**
  * Created by dell on 2016/8/1.
  */
-class StructuredStreaming {
-
+object StructuredStreaming extends BaseDataFrame{
   def worcount(session:SparkSession): Unit ={
-    val lines = session.readStream.format("socket").option("host","")
+    import session.implicits._
+    val lines = session.readStream.format("socket").option("host","").option("port","9999").load("E:\\data")
+    val words = lines.as[String].flatMap(_.split(" "))
+    val wordcount = words.groupBy("value").count()
+    wordcount.printSchema()
+
+  }
+  def main(args: Array[String]) {
+    StructuredStreaming.worcount(sparkSession)
   }
 }
